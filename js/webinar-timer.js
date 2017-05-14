0bb41e6b04988f5a2481cdd132a106fca4893c08
c5apple <c5apple_party@yahoo.co.jp>
2017-05-15 02:24:37 +0900

    ミリ秒非表示ver

--- js/webinar-timer.js
+++ js/webinar-timer.js
@@ -1,11 +1,11 @@
-/* webinar-timer.js(v1.0) */
+/* webinar-timer.js(v1.0.1) */
 window.onload = () => {
   /* 時限設定 */
   var year = 2017;
   var month = 5;
-  var date = 2;
-  var hour = 0;
-  var minute = 49;
+  var date = 15;
+  var hour = 2;
+  var minute = 24;
 
   var wbTimer = document.getElementById('wb-timer');
   var wbBenefit = document.getElementById('wb-benefit');
@@ -17,11 +17,9 @@ window.onload = () => {
     var t2 = Math.floor(((limit - time) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
     var t3 = Math.floor(((limit - time) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
     var t4 = Math.floor(((limit - time) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
-    var t5 = Math.floor(((limit - time) % (24 * 60 * 60 * 1000)) / 10) % 100;
     if (t3 < 10) t3 = '0' + t3;
     if (t4 < 10) t4 = '0' + t4;
-    if (t5 < 10) t5 = '0' + t5;
-    return `受け付け終了まで残りあと${t1}日と${t2}時間${t3}分${t4}秒${t5}`;
+    return `受け付け終了まで残りあと${t1}日と${t2}時間${t3}分${t4}秒`;
   }
   function timer(limit) {
     today = new Date().getTime();
